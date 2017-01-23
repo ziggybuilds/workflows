7b15c6bc61cde2ab8e248832554952c9655c8095
ziggybuilds <ellisjoshuag@gmail.com>
2017-01-23 17:19:19 -0500

    Gulp default

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,7 @@ gulp.task('coffee', function() {
     .pipe(gulp.dest('components/scripts'))
 });
 
-gulp.task('js', function() {
+gulp.task('js', ['coffee'], function() {
   gulp.src(jsSources)
     .pipe(concat('script.js'))
     .pipe(browserify())
@@ -39,3 +39,4 @@ gulp.task('compass', function() {
     .pipe(gulp.dest('builds/development/css'))
 });
 
+gulp.task('default', ['coffee', 'js', 'compass']);
